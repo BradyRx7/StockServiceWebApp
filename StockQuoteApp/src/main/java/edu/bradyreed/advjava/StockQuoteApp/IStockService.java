c5109128f1cff1a5c100f23d0af877cf69b1e718
Brady Reed <bradyrx7@gmail.com>
2020-08-23 12:07:19 -0400

    removing stockquote app from DI demo app project

--- StockQuoteApp/src/main/java/edu/bradyreed/advjava/StockQuoteApp/IStockService.java
+++
@@ -1,9 +0,0 @@
-package edu.bradyreed.advjava.StockQuoteApp;
-
-// Implemented by BasicStockService.java
-
-public interface IStockService {
-	
-	//Returns the value for a specified stock symbol
-	StockQuote getQuote(String symbol);
-}
