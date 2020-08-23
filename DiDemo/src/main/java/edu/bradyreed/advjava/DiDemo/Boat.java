71e3786f3c4b1327ef493b0f07ad961aba525888
Brady Reed <bradyrx7@gmail.com>
2020-08-23 12:05:48 -0400

    added Boat DI exapmple classes

--- DiDemo/src/main/java/edu/bradyreed/advjava/DiDemo/Boat.java
+++ DiDemo/src/main/java/edu/bradyreed/advjava/DiDemo/Boat.java
@@ -1,13 +1,34 @@
 package edu.bradyreed.advjava.DiDemo;
 
+import java.util.List;
+
 /**
- * Hello world!
+ * This class represents an boat
  *
  */
-public class App 
+public class Boat
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+    /**
+     * This class is an example to showcase Dependency injection
+     * 
+     * It requires several parameters at compile time and hides implementation from client code
+     * 
+     * @param engine - a list of one or more Engines on a boat
+     * @param hull - type of hull (bowrider, open, cabin, deck, pontoon)
+     * @param color - color of hull
+     */
+	private List<Engine> engines;
+	private HullEnum hull;
+	private Color color;
+	
+	
+	public Boat (List<Engine> engines, HullEnum hull, Color color) {
+		this.engines = engines;
+		this.hull = hull;
+		this.color = color;
+		
+	}
+	
 }
+
+
---
+++ DiDemo/src/main/java/edu/bradyreed/advjava/DiDemo/Color.java
@@ -0,0 +1,10 @@
+package edu.bradyreed.advjava.DiDemo;
+
+/**
+ * This class represents a concrete set of colors
+ * @author brady
+ *
+ */
+public enum Color {
+	
+}
